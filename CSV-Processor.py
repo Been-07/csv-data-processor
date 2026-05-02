72a93ada0d371fd083593a26ddd964e826744098
Been-07 <b2204284@gmail.com>
2026-05-03 00:38:33 +0330

    Update CSV-Processor.py

--- CSV-Processor.py
+++ CSV-Processor.py
@@ -1,15 +1,46 @@
+
+
+
 import csv
 
-all_fild = []
-with open("products.csv", mode="r") as file:
-    rieder = csv.DictReader(file)
-    for lines in rieder:
+
+
+def formol(data,outfail='outfail.csv'):
+        all_fild = []
+        rieder = csv.DictReader(data)
+        for lines in rieder:
+            try:
                 Price = int(lines['Price'])
                 Quantity = int(lines['Quantity'])
                 lines['total'] = Price * Quantity
                 all_fild.append(lines)
-             
-with open("outfail.csv", mode='w', newline='') as outfail:
-            write = csv.DictWriter(outfail, fieldnames=all_fild[0])
-            write.writeheader()
-            write.writerows(all_fild)
+            except(ValueError,KeyError) as Error:
+                 print(f"Error processing row: {Error}")
+                 continue
+            
+        if all_fild:    
+            with open(outfail, mode='w', newline='') as outfail:
+                write = csv.DictWriter(outfail, fieldnames=all_fild[0])
+                write.writeheader()
+                write.writerows(all_fild)
+        else:
+                 print("No data available")
+
+def main():
+    masir = input("Enter the address of the desired file, otherwise enter 0 to use the default file : ").strip()
+    if masir =="0":
+        with open("products.csv", mode="r") as file:
+                formol(file)
+    else:
+        try:
+            with open(masir, 'r') as file:
+                formol(file)
+        except FileNotFoundError:
+            print(f"File '{masir}' not found!")
+        except Exception as e:
+            print(f"Error: {e}")
+
+
+
+if __name__ == "__main__":
+     main()

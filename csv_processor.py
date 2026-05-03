158c45d89fbc4a376a4bf5bcbd1fdad128cc475b
Been-07 <b2204284@gmail.com>
2026-05-03 12:38:52 +0330

    Update csv_processor.py

--- csv_processor.py
+++ csv_processor.py
@@ -4,8 +4,17 @@
 # ORCID: 0009-0005-2756-7140            |
 # ======================================|
 import csv
+import os
 
-def formol(data,outfail='outfail.csv'):
+def get_next_outfail(base = "outfail"):
+     counter = 1
+     while True:
+          fail_name = f"{base}_{counter}.csv"
+          if not os.path.exists(fail_name):
+               return fail_name
+          counter +=1
+
+def formol(data,outfail):
         all_fild = []
         rieder = csv.DictReader(data)
         for lines in rieder:
@@ -23,24 +32,57 @@ def formol(data,outfail='outfail.csv'):
                 write = csv.DictWriter(outfail, fieldnames=all_fild[0])
                 write.writeheader()
                 write.writerows(all_fild)
+            print(f"Seve to {outfail}")
+            return True
         else:
-                 print("No data available")
+            print("No data available")
+            return False
 
+def get_failname():
+    while True:
+        print("=" * 130)
+        print("Enter the address of the desired file, otherwise enter 0 to use the default file (products.csv) or type exit to exit the program")
+        print("=" * 130)
+        address_user = input("\nPlease enter the address (for exit: exit, for default address: 0): ")
+        if address_user.upper() == "EXIT":
+            return None
+        if address_user == "0":
+            file_name = "products.csv"
+        else:
+            file_name = address_user
+
+        if os.path.exists(file_name):
+            return file_name
+        else:
+            print(f"File{file_name} not found, try again")
+                        
 def main():
-    masir = input("Enter the address of the desired file, otherwise enter 0 to use the default file : ").strip()
-    if masir =="0":
-        with open("products.csv", mode="r") as file:
-                formol(file)
-    else:
+    print("=" * 130)
+    print("                                             CSV-Data-Processor")
+    print("                                             Calculates Price × Quantity = Total")
+    print("=" * 130)
+    counter = 1
+    while True:
+        print(f"--- Processing Session_{counter} ---")
+        deta = get_failname()
+        if deta is None:
+            print("bye bye")
+            break
+        outfail = get_next_outfail()
+        print(f"File saved with name {outfail}")
         try:
-            with open(masir, 'r') as file:
-                formol(file)
-        except FileNotFoundError:
-            print(f"File '{masir}' not found!")
-        except Exception as e:
-            print(f"Error: {e}")
+            with open(deta,mode="r") as file:
+                formol(file,outfail)
+        except Exception as error:
+            print(f"Error File: {error}")
+        counter += 1
+        print("\n" + "-" * 100)
+        again = input("Do you want to process another file? (Yes/No): ").strip().upper()
+        if again != "YES":
+            print("bye bye")
+            break
 
 
 
 if __name__ == "__main__":
-     main()
+    main()
